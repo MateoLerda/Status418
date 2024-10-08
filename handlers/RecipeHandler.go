686e4e4ba7dc030324b34b019c71dca9d99c4958
MateoLerda <Mateolerda03@gmail.com>
2024-10-08 17:38:39 -0300

    Advances recipeHandler

--- handlers/RecipeHandler.go
+++ handlers/RecipeHandler.go
@@ -4,6 +4,7 @@ import (
 	"Status418/dto"
 	"Status418/services"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,28 +14,28 @@ type RecipeHandler struct {
 
 func NewRecipeHandler(rs services.RecipeServiceInterface) *RecipeHandler {
 	return &RecipeHandler{
-		rs : rs,
+		rs: rs,
 	}
 }
 
-func (rh *RecipeHandler) GetAll(c *gin.Context){
+func (rh *RecipeHandler) GetAll(c *gin.Context) {
 	userId := c.Param("userId")
 	var filters dto.FiltersDto
 	err := c.ShouldBindJSON(&filters)
-	if err != nil{
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get filters", "Moment": filters.Moment})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get filters"})
 	}
 
-	recipes, err := rh.rs.GetAll(userId,filters)
+	recipes, err := rh.rs.GetAll(userId, filters)
 
-	if err.Error()== "internal"{
+	if err.Error() == "internal" {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed to get recipes from database",
 		})
 		return
 	}
 
-	if err.Error() == "nocontent"{
+	if err.Error() == "nocontent" {
 		c.JSON(http.StatusNoContent, gin.H{
 			"error": "Not found any recipe",
 		})
@@ -42,14 +43,57 @@ func (rh *RecipeHandler) GetAll(c *gin.Context){
 	}
 	c.JSON(http.StatusOK, recipes)
 }
-func (rh *RecipeHandler) Create(c *gin.Context){
+func (rh *RecipeHandler) Create(c *gin.Context) {
+	var recipe dto.RecipeDto
+	err := c.ShouldBindJSON(&recipe)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-} 
+	res, err := rh.rs.Create(recipe)
 
-func (rh *RecipeHandler) Delete(c *gin.Context){
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create recipe" + err.Error()})
+	}
 
+	c.JSON(http.StatusOK, res)
 }
 
-func (rh *RecipeHandler) Update(c *gin.Context){
+func (rh *RecipeHandler) Delete(c *gin.Context) {
+	id := c.Param("id")
+	res, err := rh.rs.Delete(id)
 
+	if err.Error() == "internal" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete recipe" + id})
+		return
+	}
+
+	if err.Error() == "notfound" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Not found any recipe with id:" + id})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func (rh *RecipeHandler) Update(c *gin.Context) {
+	var recipe dto.RecipeDto
+
+	err:= c.ShouldBindJSON(&recipe)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := rh.rs.Update(recipe)
+
+	if err.Error() == "internal"{
+		c.JSON(http.StatusInternalServerError, gin.H{"erro": "Failed to delete recipe"})
+	}
+	if err.Error() == "notfound"{
+		c.JSON(http.StatusNotFound,  gin.H{"error": "Not found any recipe"})
+	}
+
+	c.JSON(http.StatusOK, res)
 }
--- main.go
+++ main.go
@@ -11,11 +11,11 @@ import (
 )
 
 var (
-	r *gin.Engine
-	foodHandler *handlers.FoodHandler
+	r               *gin.Engine
+	foodHandler     *handlers.FoodHandler
 	purchaseHandler *handlers.PurchaseHandler
-	userHandler *handlers.UserHandler
-	recipeHandler *handlers.RecipeHandler
+	userHandler     *handlers.UserHandler
+	recipeHandler   *handlers.RecipeHandler
 )
 
 func main() {
@@ -23,9 +23,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	r = gin.Default()
-		
+
 	routes()
 	dependencies()
 
@@ -33,30 +33,30 @@ func main() {
 }
 
 func routes() {
-	foodRoutes := r.Group("/foods") 
+	foodRoutes := r.Group("/foods")
 	foodRoutes.GET("/:userId", foodHandler.GetAll)
 	foodRoutes.GET("/:userId/:code", foodHandler.GetByCode)
 	foodRoutes.POST("/", foodHandler.Create)
 	foodRoutes.DELETE("/:code", foodHandler.Delete)
 	foodRoutes.PUT("/:code", foodHandler.Update)
-	
+
 	purchaseRoutes := r.Group("/purchases")
 	purchaseRoutes.POST("/:userId")
 	purchaseRoutes.GET("/:userId")
 
-	userRoutes:= r.Group("/users")
+	userRoutes := r.Group("/users")
 	userRoutes.GET("/")
 	userRoutes.GET("/:userId")
 	userRoutes.POST("/")
-	userRoutes.PUT("/:userId")
-	userRoutes.DELETE("/:userId")	
+	userRoutes.PUT("/:userId") //aca manda un user id que no usa
+	userRoutes.DELETE("/:userId")
 
 	recipesRoutes := r.Group("/recipes")
-	recipesRoutes.GET("/:userId")
-	recipesRoutes.DELETE("/:id")
-	recipesRoutes.PUT("/:id")
-	recipesRoutes.POST("/")
-	
+	recipesRoutes.GET("/:userId", recipeHandler.GetAll)
+	recipesRoutes.DELETE("/:id", recipeHandler.Delete)
+	recipesRoutes.PUT("/", recipeHandler.Update)
+	recipesRoutes.POST("/", recipeHandler.Create)
+
 }
 
 func dependencies() {
@@ -66,7 +66,7 @@ func dependencies() {
 	var foodService services.FoodServiceInterface
 
 	db = repositories.NewMongoDB()
-	
+
 	foodRepository = repositories.NewFoodRepository(db)
 	foodService = services.NewFoodService(foodRepository)
 	foodHandler = handlers.NewFoodHandler(foodService)
@@ -77,17 +77,16 @@ func dependencies() {
 	purchaseRepository = repositories.NewPurchaseRepository(db)
 	purchaseService = services.NewPurchaseService(purchaseRepository)
 	purchaseHandler = handlers.NewPurchaseHandler(purchaseService)
-	
+
 	var userRepository repositories.UserRepositoryInterface
 	var userService services.UserServiceInterface
 
 	userRepository = repositories.NewUserRepository(db)
 	userService = services.NewUserService(userRepository)
 	userHandler = handlers.NewUserHandler(userService)
-	
+
 	var recipeRepository repositories.RecipeRepositoryInterface
 	var recipeService services.RecipeServiceInterface
-	
 
 	recipeRepository = repositories.NewRecipeRepository(db)
 	recipeService = services.NewRecipeService(recipeRepository)
